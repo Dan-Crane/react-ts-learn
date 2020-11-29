6ebd5a62ed4c391ee3b5108e80bc51e831eb9b41
dan <gre4kaDISCOUNT@gmail.com>
2020-11-29 21:22:22 +0900

    типизация счетчика

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+type TitleProps = {
+  title: string
+}
+
+let Title: React.FC<TitleProps> = ({title, children}) => <><h1>{title}</h1>
+  {children}
+</>
+
+type CounterProps = {
+  title?: string
+}
+type CounterState = {
+  count: number
+}
+
+class Counter extends React.Component<CounterProps, CounterState> {
+  constructor(props: CounterProps) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  handelClick = (e : React.MouseEvent<HTMLButtonElement>) : void => {
+    console.log(e.clientX, e.clientY)
+    this.setState(({count}) => ({
+      count: ++count
+    }))
+  }
+
+  render() {
+    return <>
+      <h1>{this.props.title}{this.state.count}</h1>
+      <button onClick={this.handelClick}>Click</button>
+    </>
+  }
+}
+
+class Form extends React.Component<{}, {}>{
+  handelFocus = (e : React.FocusEvent<HTMLInputElement>) : void => {
+    console.log(e.target.value)
+  }
+
+  handelSubmit = (e : React.FormEvent<HTMLFormElement>) : void => {
+    e.preventDefault()
+    console.log('Submit')
+  }
+  handelCopy = (e : React.ClipboardEvent<HTMLInputElement>) : void => {
+    console.log(e.clipboardData.effectAllowed)
+  }
+
+  render(): React.ReactNode {
+    return (
+      <form onSubmit={this.handelSubmit}>
+        <label>
+          Simple text:
+          <input
+            onFocus={this.handelFocus}
+            onCopy={this.handelCopy}
+            type='text'
+            name='text'
+          />
+          <button>
+            submit
+          </button>
+        </label>
+      </form>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title title='test'>
+        <div>children</div>
+      </Title>
+      <Counter title='Count:'/>
+      <Form/>
     </div>
   );
 }
